40d35db39113d076057622b9c1390227d2370344
Jakub Spólnik <jakubspolnik@student.agh.edu.pl>
2023-12-05 00:34:18 +0100

    SR-5 | Add solution for lab 5

--- src/main/scala/EShop/lab5/Payment.scala
+++ src/main/scala/EShop/lab5/Payment.scala
@@ -2,6 +2,7 @@ package EShop.lab5
 
 import EShop.lab2.TypedCheckout
 import EShop.lab3.OrderManager
+import EShop.lab3.OrderManager.ConfirmPaymentReceived
 import EShop.lab5.Payment.{PaymentRejected, WrappedPaymentServiceResponse}
 import EShop.lab5.PaymentService.{PaymentClientError, PaymentServerError, PaymentSucceeded}
 import akka.actor.typed.{ActorRef, Behavior, ChildFailed, SupervisorStrategy}
@@ -20,22 +21,38 @@ object Payment {
   case object PaymentRejected extends Response
 
   val restartStrategy = SupervisorStrategy.restart.withLimit(maxNrOfRetries = 3, withinTimeRange = 1.second)
-
   def apply(
     method: String,
     orderManager: ActorRef[OrderManager.Command],
     checkout: ActorRef[TypedCheckout.Command]
   ): Behavior[Message] =
     Behaviors
-      .receive[Message](
-        (context, msg) =>
-          msg match {
-            case DoPayment                                       => ???
-            case WrappedPaymentServiceResponse(PaymentSucceeded) => ???
+      .receive[Message]((context, msg) =>
+        msg match {
+          case DoPayment =>
+            val paymentResponseMapper: ActorRef[PaymentService.Response] =
+              context.messageAdapter(rsp => WrappedPaymentServiceResponse(rsp))
+
+            val supervisor = Behaviors.supervise(PaymentService(method, paymentResponseMapper))
+                  .onFailure[PaymentClientError](restartStrategy)
+
+
+            val secondSupervisor = context.spawnAnonymous(
+              Behaviors.supervise(supervisor)
+                  .onFailure[PaymentServerError](SupervisorStrategy.restart)
+            )
+            context.watch(secondSupervisor)
+            Behaviors.same
+          case WrappedPaymentServiceResponse(PaymentSucceeded) =>
+            orderManager ! ConfirmPaymentReceived
+            checkout ! TypedCheckout.ConfirmPaymentReceived
+            Behaviors.stopped
         }
       )
       .receiveSignal {
-        case (context, Terminated(t)) => ???
+        case (context, Terminated(t)) =>
+          notifyAboutRejection(orderManager, checkout)
+          Behaviors.stopped
       }
 
   // please use this one to notify when supervised actor was stoped
